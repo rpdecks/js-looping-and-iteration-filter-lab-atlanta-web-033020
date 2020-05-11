4ad510dbafb83e3f5691222d7448e7385d4af8d0
rpdecks <rpdecks@gmail.com>
2020-05-11 12:29:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(drivers, name){
+    return drivers.filter(function (driverName) {
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, letters) {
+    let nameTest = letters.length;
+    return drivers.filter (function (name) {
+        return name.slice(0, nameTest) === letters;
+    });
+}
+
+function matchName (drivers, string) {
+    return drivers.filter(function (driverName) {
+        return driverName.name === string;
+    });
+}
